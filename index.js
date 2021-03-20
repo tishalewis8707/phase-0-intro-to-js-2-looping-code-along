19e1714814dcbc06d9e88534ac5764e2fe85694a
tishalewis8707 <tishalewis8707@gmail.com>
2021-03-19 23:22:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"] ;
+const suprise = ["birthday"]
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+
+  
+    console.log(`Thank you, ${cards[i]} for the wonderful ${suprise} gift!`);
+    debugger;
+  }
+
+  return cards;
+}
+
+ writeCards(cards);
+
+
+ let countup = 0;
+ while (countup < 10) {
+   console.log(countup++);
+ }
